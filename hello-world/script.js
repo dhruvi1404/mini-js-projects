82cf74999499c6a8994081a1ec0afe1b8f58f147
Dhruvi Akhilesh Shah <dh368867@dal.ca>
2025-04-05 17:33:49 -0300

    learning-hello-world

---
+++ hello-world/script.js
@@ -0,0 +1,5 @@
+const width = +prompt('Please enter width of rectangle')
+const height = +prompt('Please enter height of rectangle')
+
+console.log('Area is : ' + width*height);
+document.write(`Reactangle Area : ${width*height}`)
