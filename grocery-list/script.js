e6e27848750919d60c58857e576db948194a1f9e
Dhruvi Akhilesh Shah <dh368867@dal.ca>
2025-04-05 17:40:50 -0300

    more structured way

--- grocery-list/script.js
+++ grocery-list/script.js
@@ -1,15 +1,25 @@
-let itemName = document.querySelector("#name");
-let addBtn = document.querySelector("#add");
-let ulList = document.querySelector("#shopping-list");
+let items = [];
+
+let input = document.querySelector("#itemInput");
+let addBtn = document.querySelector("#addBtn");
+let ulList = document.querySelector("#groceryList");
 
 addBtn.addEventListener("click", ()=>{
-    let value = itemName.value.trim();
+    let value = input.value.trim();
+
     if(value!= " "){
-        let listItem = document.createElement('li');
-        listItem.textContent = value;
-        ulList.appendChild(listItem); 
-        itemName.value = "";
+        items.push(value);
+        renderList(); 
+        input.value = "";
     }
-    
-    
 });
+
+function renderList(){
+    ulList.innerHTML = ""; // Clear old list
+
+    items.forEach(item => {
+      const li = document.createElement("li");
+      li.textContent = item;
+      ulList.appendChild(li);
+    });
+}
